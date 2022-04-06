39f5fde75e498b893571b40e38f2e6b4a9518e36
krishansingh1 <kkrishan7074@gmail.com>
2022-04-06 10:53:19 +0530

    Commit

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-document.body.style.backgroundColor = "#f15025";
-document.getElementById('text').textContent='#F15025';
+const colors = ['RED', 'GREEN', 'RGBA(133,122,200)','#F15025'];
 
-const clickforChange=document.getElementById('clicked');
+document.body.style.backgroundColor = "#f1f5f8";
+document.getElementById('text').textContent = '#F1F5F8';
 
-clickforChange.addEventListener('click',()=>{
-    document.body.style.backgroundColor = "red";
-    document.getElementById('text').textContent='RED';
-})
+const clickforChange = document.getElementById('clicked');
 
-clickforChange.addEventListener('click',()=>{
-    document.body.style.backgroundColor = "rgba(133,122,200)";
-    document.getElementById('text').textContent='RGBA(133,122,200)';
-})
+clickforChange.addEventListener('click', () => {
+    const random=getRandomNumber();
+
+    document.body.style.backgroundColor = colors[random];
+    document.getElementById('text').textContent = colors[random];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
+console.log(getRandomNumber());
